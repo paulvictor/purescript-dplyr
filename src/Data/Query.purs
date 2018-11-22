90d7b47e0f59cd132791360bd502ebc961f53709
Paul Victor Raj <paulvictor@gmail.com>
2018-11-22 11:28:28 +0530

    Adding a way to compose queries

--- src/Data/Query.purs
+++ src/Data/Query.purs
@@ -2,8 +2,9 @@ module Data.Query where
 
 import Prelude
 
-import Control.Monad.Reader.Class (asks)
-import Control.Monad.Reader.Trans (ReaderT)
+import Control.Monad.Reader.Class (ask, asks)
+import Control.Monad.Reader.Trans (ReaderT, runReaderT)
+import Control.Monad.Trans.Class (lift)
 import Data.DataFrame (DataFrame(..))
 import Data.Filterable (filter) as Filterable
 import Data.Foldable (foldr)
@@ -16,6 +17,17 @@ import Data.Tuple (snd)
 import Data.Tuple.Nested (type (/\))
 
 type Query m a b = ReaderT (DataFrame a) m (DataFrame b)
+
+-- We should make (Query m) a Semigroupoid and use `compose` instead of this way
+composeQuery
+  :: ∀ m a b c
+  . Monad m
+ => Query m a b
+ -> Query m b c
+ -> Query m a c
+composeQuery q1 q2 = do
+  df <- ask
+  lift $ runReaderT q1 df >>= runReaderT q2
 
 filter :: ∀ m a. Monad m => (a -> Boolean) -> Query m a a
 filter f = asks (Filterable.filter f)
--- test/Main.purs
+++ test/Main.purs
@@ -1,6 +1,11 @@
 module Test.Main where
 
 import Prelude
+
+import Control.Monad.Reader.Class (ask)
+import Control.Monad.Reader.Trans (runReaderT)
+import Control.Monad.Trans.Class (lift)
+import Data.Query (Query)
 import Effect (Effect)
 import Effect.Console (log)
 
